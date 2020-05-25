8ae274d9dae41689088a2fb6b676f32f96937f4c
Luis <lmillan131@gmail.com>
2020-05-25 10:30:26 -0400

    finalización

--- Product_prices_limit/__manifest__.py
+++ Product_prices_limit/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': "Product_prices_limit",
-    'description': "Modifica la visibilidad de precios y tarifas de productos",
+    'description': "Modifica la visibilidad de tarifas",
     'author': "Luis Millan",
     'depends': ['sale'],
     'data': ['security/product_prices_security.xml', 'views/product_prices_view.xml'],
--- Product_prices_limit/models/mod_prod.py
+++ Product_prices_limit/models/mod_prod.py
@@ -1,4 +1,5 @@
 #-*- coding: utf-8 -*-
+from odoo import models, fields, api
 class Prices_product(models.Model):
     _inherit ='product.pricelist'
     security_groups = fields.Many2one('res.groups', 'security groups')
